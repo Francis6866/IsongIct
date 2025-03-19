45229b11af65d204fe6c0d3a787b703fd4ef72bf
Francis james <ibolokefrancisjames2@gmail.com>
2025-03-19 22:10:02 +0100

    Modified ui, fixed position, more reactive menu

--- js/main.js
+++ js/main.js
@@ -1,32 +1,48 @@
 'use strict';
-const nav = document.querySelector('.nav')
-const toggler = document.querySelector('.nav__tog')
 
-function showMenu() {
+// const swiper = new Swiper('.swiper', {
+//     autoplay: {
+//         delay: 5000,
+//         disableOnInteraction: false
+//     },
+//     loop: true,
+
+//     // If we need pagination
+//     // pagination: {
+//     //     el: '.swiper-pagination',
+//     // },
+
+//     // Navigation arrows
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+
+// });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const swiper = new Swiper(".swiper", {
+        autoplay: { delay: 4000 },
+        loop: true,
+        navigation: { nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" },
+    });
+
+    // nav toggler
+    const nav = document.querySelector('.nav')
+    const toggler = document.querySelector('.nav__tog')
+
+    function showMenu() {
     nav.classList.toggle('collapse')
-}
+    }
 
-toggler.addEventListener('click', showMenu)
+    toggler.addEventListener('click', showMenu)
 
-
-
-const swiper = new Swiper('.swiper', {
-    autoplay: {
-        delay: 5000,
-        disableOnInteraction: false
-    },
-    loop: true,
-
-    // If we need pagination
-    // pagination: {
-    //     el: '.swiper-pagination',
-    // },
-
-    // Navigation arrows
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-
-
+     // Close mobile menu on link click
+     const navLinks = document.querySelectorAll(".nav__link");
+     navLinks.forEach(link => {
+         link.addEventListener("click", () => {
+             nav.classList.toggle("collapse");
+         });
+        })
 });
